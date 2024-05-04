01905be77e6b6e18f7df6dd36b5c9a17e391ce23
djars500 <djars500@gmail.com>
2024-05-04 15:12:42 +0500

    allowed host

--- core1/settings.py
+++ core1/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'django-insecure-c1z+2nzblp#0*ax*j+804nmlaxfnt&$2f*@&@4u_rmk-+1xk*n
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 # Application definition
 
